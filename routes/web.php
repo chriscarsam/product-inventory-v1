14aa32c74d1c6dd6ba0192f2be6dd8270c17f608
ubuntu <ubuntu@gmail.com>
2019-09-23 22:10:31 -0500

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,10 @@ Route::get('/', function(){
     return 'Home';
 });
 
-Route::get('/usuarios', function () {
-    return 'Usuarios';
-});
+Route::get('/usuarios', 'UserController@index');
 
-Route::get('/usuarios/{id}', function ($id) {
-    return "Mostrar detalle del usuario: {$id} ";
-})->where('id', '[0-9]+');
+Route::get('/usuarios/{id}', 'UserController@show')->where('id', '[0-9]+');
 
-Route::get('/usuarios/nuevo', function () {
-    return 'Crear nuevo usuario';
-});
+Route::get('/usuarios/nuevo', 'UserController@create');
 
-Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
-
-    $name = ucfirst($name);
-
-    if ($nickname) {
-        return "Bienvenido {$name}, tu apodo es {$nickname}";
-    } else {
-        return "No tinenes apodo {$name}";
-    }
-});
+Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
