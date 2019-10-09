97dbe83bfda528b6b128668360480c6c7b687752
ubuntu <chriscar.sam@yahoo.com>
2019-10-09 18:08:15 -0500

    Second seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,10 +12,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ?',['Desarrollador back-end']);
+
+        $professionId = DB::table('professions')
+                    ->whereTitle('Desarrollador back-end')
+                    ->value('id');
+
+        
         DB::table('users')->insert([
             'name' => 'Christian Sambachi',
             'email' => 'chris@gmail.com',
             'password' => bcrypt('laravel'),
+            'profession_id' => $professionId,
         ]);
     }
 }
